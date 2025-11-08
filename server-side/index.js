5e43ea9d6d384956eafe11595b66edcf209cc806
Ayan <sujon.seo.bd@gmail.com>
2025-11-08 12:26:29 +0600

    added mongodb

--- server-side/index.js
+++ server-side/index.js
@@ -1,1 +1,101 @@
-console.log('server added');
+
+// assignment-B12A10
+// EmCSTbirhFUjKOXP
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// mongoDB URI and Client 
+const uri = "mongodb+srv://assignment-B12A10:EmCSTbirhFUjKOXP@cluster0.tachgq7.mongodb.net/?appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+
+app.get('/', (req, res)=>{
+  res.send("Server is running");
+})
+
+
+
+
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // All collection and Database
+    const db = client.db("assignment-B12A10");
+    const usersCollention = db.collection("users");
+    
+
+    // ALl Methords 
+    app.post('/users', async (req, res)=>{
+      const newUser = req.body;
+      const result = await usersCollention.insertOne(newUser);
+      res.send(result);
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.listen(port, ()=>{
+  console.log(`Server is running on port: ${port}`);
+})
+
